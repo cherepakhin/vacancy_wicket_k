e95f7c5a598efb955395c27c35cad86f0b1ca0ab
Черепахин Василий <vasi.che@gmail.com>
2025-03-02 20:02:39 +0500

    ProjectRestTemplateIntegrationTest

--- src/main/kotlin/ru/perm/v/vacancy/service/ProjectRestTemplate.kt
+++ src/main/kotlin/ru/perm/v/vacancy/service/ProjectRestTemplate.kt
@@ -1,31 +1,21 @@
 package ru.perm.v.vacancy.service
 
 import org.slf4j.LoggerFactory
-import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import ru.perm.v.vacancy.dto.CompanyDto
 
+/**
+ * Demo use RestTemplate
+ */
 @Service
 class ProjectRestTemplate {
 
     private val logger = LoggerFactory.getLogger(this.javaClass.name)
 
-    fun getForObjectList(url: String): Any {
-// for list
-//        val response = RestTemplate().getForObject(url, Array<CompanyDto>::class.java).toList()
-
-//      РАБОТАЕТ!
-//      val response = RestTemplate().getForObject(url, CompanyDto::class.java)
-
-//        val response = RestTemplate().getForObject(url, Array<CompanyDto>::class.java)
-        val response = RestTemplate().getForEntity(url, List::class.java).body as List<CompanyDto>
-//        val response = RestTemplate().exchange<List<CompanyDto>>(url, HttpMethod.GET,
-//            null, object : ParameterizedTypeReference<List<CompanyDto>>() {})
-        return response
-    }
-
+    /**
+     * Demo use RestTemplate() for return OBJECT
+     */
     fun getForObjectCompany(url: String): CompanyDto? {
 // for list
 //        val response = RestTemplate().getForObject(url, Array<CompanyDto>::class.java).toList()
@@ -38,7 +28,21 @@ class ProjectRestTemplate {
         return response
     }
 
-//    fun getForEntity(url: String): ResponseEntity {
-//        RestTemplate().getForEntity(url)
-//    }
+    /**
+     * Demo use RestTemplate() for return LIST OBJECT
+     */
+    fun getForObjectList(url: String): Any {
+
+//      val response = RestTemplate().getForObject(url, CompanyDto::class.java)
+
+// for list
+//      val response = RestTemplate().getForObject(url, Array<CompanyDto>::class.java).toList()
+//      val response = RestTemplate().getForObject(url, Array<CompanyDto>::class.java)
+//      val response = RestTemplate().exchange<List<CompanyDto>>(url, HttpMethod.GET,
+//              null, object : ParameterizedTypeReference<List<CompanyDto>>() {})
+
+        val response = RestTemplate().getForEntity(url, List::class.java).body as List<CompanyDto>
+        return response
+    }
+
 }
--- src/test/kotlin/ru/perm/v/vacancy/service/ProjectRestTemplateIntegrationTest.kt
+++ src/test/kotlin/ru/perm/v/vacancy/service/ProjectRestTemplateIntegrationTest.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.web.client.RestTemplate
 import ru.perm.v.vacancy.dto.CompanyDto
 
 @SpringBootTest
@@ -31,4 +32,19 @@ class ProjectRestTemplateIntegrationTest {
         assertEquals(CompanyDto(COMPANY_N, "COMPANY_1"), response)
     }
 
+    @Test
+    fun forList() {
+        val url = "http://127.0.0.1:8980/vacancy/api/company/"
+        val companies = RestTemplate().getForEntity(url, List::class.java).body as List<CompanyDto>
+
+        assertEquals(4, companies.size)
+    }
+
+    @Test
+    fun forEntity() {
+        val url = "http://127.0.0.1:8980/vacancy/api/company/1"
+        val company = RestTemplate().getForEntity(url, CompanyDto::class.java).body as CompanyDto
+
+        assertEquals(1, company.n)
+    }
 }
